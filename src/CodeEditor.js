0370b1d151748c87e8900d378756a4e707bab14d
Renildo Pereira <rrenildopereiraa@gmail.com>
2023-12-26 16:23:55 +0000

    Codebase improvements

---
+++ src/CodeEditor.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import Editor from '@monaco-editor/react';
+
+const CodeEditor = ({ code, onChange }) => {
+    const handleEditorDidMount = (editor, monaco) => {
+        monaco.editor.defineTheme('vortyx', {
+            base: 'vs-dark',
+            inherit: true,
+            rules: [
+                { token: 'tag', foreground: '#855fa8' },
+                { token: 'attribute.name', foreground: '#a863a8' },
+                { token: 'attribute.value', foreground: '#ffffff' },
+                { token: 'string', foreground: '#ffffff' },
+                { token: 'delimiter.angle', foreground: '#dbbcd6' },
+                { token: 'delimiter.curly', foreground: '#dbbcd6' },
+                { token: 'delimiter.square', foreground: '#dbbcd6' },
+                { token: 'keyword', foreground: '#775271' },
+                { token: 'number', foreground: '#775271' },
+                { token: 'meta.tag', foreground: '#775271' },
+                { token: 'meta.tag.custom', foreground: '#775271' },
+                { token: 'meta.tag.doctype', foreground: '#775271' },
+                { token: 'meta.tag.inline', foreground: '#775271' },
+                { token: 'meta.tag.script', foreground: '#775271' },
+                { token: 'meta.tag.style', foreground: '#775271' },
+                { token: 'meta.tag.xml', foreground: '#775271' },
+                { token: 'comment', foreground: '#696a72' },
+            ],
+            colors: {
+                'editor.background': '#282a36',
+                'editorSuggestWidget.background': '#282a36',
+                'editorHoverWidget.background': '#282a36',
+                'editorFindWidget.background': '#282a36',
+                'editorFindWidget.input.background': '#282a36',
+                'editor.foreground': '#dbbcd6',
+                'editor.cursorColor': '#ffffff',
+                'editor.lineHighlightBackground': '#20222b',
+                'editor.selectionBackground': '#3e3f4a'
+            },
+        });
+        
+        monaco.editor.setTheme('vortyx');
+    };
+
+    return (
+        <Editor
+            height="100vh"
+            language="html"
+            value={code}
+            options={{
+                language: 'html',
+                minimap: {
+                    enabled: false
+                }
+            }}
+            onChange={onChange}
+            onMount={handleEditorDidMount}
+        />
+    );
+};
+
+export default CodeEditor;
--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import Editor from '@monaco-editor/react';
+import CodeEditor from './CodeEditor';
 
 const Home = () => {
     const yummacss = '<link rel="stylesheet" href="https://unpkg.com/yummacss/public/css/yumma.min.css" crossorigin="anonymous">';
@@ -23,62 +23,10 @@ const Home = () => {
         setCode(newValue);
     };
 
-    function handleEditorDidMount(editor, monaco) {
-        
-        monaco.editor.defineTheme('vortyxTheme', {
-            base: 'vs-dark',
-            inherit: true,
-            rules: [
-                { token: 'tag', foreground: '#855fa8' },
-                { token: 'attribute.name', foreground: '#a863a8' },
-                { token: 'attribute.value', foreground: '#ffffff' },
-                { token: 'string', foreground: '#ffffff' },
-                { token: 'delimiter.angle', foreground: '#dbbcd6' },
-                { token: 'delimiter.curly', foreground: '#dbbcd6' },
-                { token: 'delimiter.square', foreground: '#dbbcd6' },
-                { token: 'keyword', foreground: '#775271' },
-                { token: 'number', foreground: '#775271' },
-                { token: 'meta.tag', foreground: '#775271' },
-                { token: 'meta.tag.custom', foreground: '#775271' },
-                { token: 'meta.tag.doctype', foreground: '#775271' },
-                { token: 'meta.tag.inline', foreground: '#775271' },
-                { token: 'meta.tag.script', foreground: '#775271' },
-                { token: 'meta.tag.style', foreground: '#775271' },
-                { token: 'meta.tag.xml', foreground: '#775271' },
-                { token: 'comment', foreground: '#696a72' },
-            ],
-            colors: {
-                'editor.background': '#282a36',
-                'editorSuggestWidget.background': '#282a36',
-                'editorHoverWidget.background': '#282a36',
-                'editorFindWidget.background': '#282a36',
-                'editorFindWidget.input.background': '#282a36',
-                'editor.foreground': '#dbbcd6',
-                'editor.cursorColor': '#ffffff',
-                'editor.lineHighlightBackground': '#20222b',
-                'editor.selectionBackground': '#3e3f4a'
-            },
-        });
-        monaco.editor.setTheme('vortyxTheme');
-    }
-
     return (
         <div className="editor-container">
             <div className="editor-wrapper">
-                <Editor
-                    height="100vh"
-                    language="html"
-                    value={code}
-                    options={{
-                        language: 'html',
-                        fontFamily: 'Consolas !important',
-                        minimap: {
-                            enabled: false
-                        }
-                    }}
-                    onChange={onChange}
-                    onMount={handleEditorDidMount}
-                />
+                <CodeEditor code={code} onChange={onChange} />
             </div>
             <div className="preview-container">
                 <iframe
