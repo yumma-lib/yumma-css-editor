a5c21b9bfe5b923f0714e2c43f90a50722420aad
Renildo Pereira <rrenildopereiraa@gmail.com>
2024-01-10 15:18:43 +0000

    Updated Navbar component

--- src/Navbar.js
+++ src/Navbar.js
@@ -3,11 +3,11 @@ const Navbar = () => {
         <div>
 
             <main className="nav-white">
-                <p className="fw-500">Try Yumma CSS</p>
+                <p className="t-violet fw-500">Try Yumma CSS</p>
                 <ul className="d-f">
-                    <li className="ml-1 t-magenta" aria-current="page"><a href="/">Home</a></li>
-                    <li className="ml-1 t-black h:t-magenta"><a target="_blank" rel="noreferrer" href="https://yummacss.vercel.app/">Docs</a></li>
-                    <li className="ml-1 t-black h:t-magenta"><a target="_blank" rel="noreferrer" href="https://github.com/yumma-lib/yumma-css-editor#readme">About</a></li>
+                    <li className="ml-1 t-violet" aria-current="page"><a href="/">Home</a></li>
+                    <li className="ml-1 t-black h:t-violet"><a target="_blank" rel="noreferrer" href="https://yummacss.vercel.app/">Docs</a></li>
+                    <li className="ml-1 t-black h:t-violet"><a target="_blank" rel="noreferrer" href="https://github.com/yumma-lib/yumma-css-editor#readme">About</a></li>
                 </ul>
             </main>
         </div>
